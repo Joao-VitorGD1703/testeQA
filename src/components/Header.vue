9e5eedd37f239699fb37dad2aaa3f02a6bba4d85
João Vítor Guimarães Douverny <77338020+Joao-VitorGD1703@users.noreply.github.com>
2024-06-01 00:16:12 -0300

    add logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,7 +35,7 @@ const toggleFocus = () => {
   <header class="cont-princ">
     <nav class="navbar navbar-expand-lg navbar-dark" aria-label="Fifth navbar example">
       <div class="container-fluid">
-        <a class="navbar-brand title" href="#">TesteQA</a>
+        <a class="navbar-brand title"  @click="useGoTo('/cnpj')" >TesteQA</a>
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
